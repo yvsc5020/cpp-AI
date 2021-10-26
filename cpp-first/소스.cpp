f0910f5156960a9b6e9d1111415dce2d7ce836bb
KwonWooMin <dnalsdnwls@gmail.com>
2021-10-26 17:47:12 +0900

    code-change

--- cpp-first/소스.cpp
+++ cpp-first/소스.cpp
@@ -16,8 +16,31 @@ int main() {
 
 	pList = insertNode(pList, 'A');
 	pList = insertNode(pList, 'B');
+	pList = insertNode(pList, 'C');
 	pList = insertNode(pList, 'D');
 	pList = insertNode(pList, 'E');
+	pList = insertNode(pList, 'F');
+	pList = insertNode(pList, 'G');
+	pList = insertNode(pList, 'H');
+	pList = insertNode(pList, 'I');
+	pList = insertNode(pList, 'J');
+	pList = insertNode(pList, 'K');
+	pList = insertNode(pList, 'L');
+	pList = insertNode(pList, 'M');
+	pList = insertNode(pList, 'N');
+	pList = insertNode(pList, 'O');
+	pList = insertNode(pList, 'P');
+	pList = insertNode(pList, 'Q');
+	pList = insertNode(pList, 'R');
+	pList = insertNode(pList, 'S');
+	pList = insertNode(pList, 'T');
+	pList = insertNode(pList, 'U');
+	pList = insertNode(pList, 'V');
+	pList = insertNode(pList, 'W');
+	pList = insertNode(pList, 'X');
+	pList = insertNode(pList, 'Y');
+	pList = insertNode(pList, 'Z');
+
 	PrintList(pList);
 
 	return 0;
